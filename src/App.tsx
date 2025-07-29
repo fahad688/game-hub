08e8c8ad0a5a55ffe932552b60b1a14acda2eb89
fahad <fahad.akbar@clearpathortho.com>
2025-07-29 18:50:03 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { Button, Box } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box p={4}>
+      <Button colorScheme="teal">It works!</Button>
+    </Box>
+  );
+}
+
+export default App;
